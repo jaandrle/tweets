f3fbd4d09aa0c53e8b00aa1494b344f6a5c25ccc
Jan Andrle <andrle.jan@centrum.cz>
2022-11-06 19:37:15 +0100

    :tada:

---
+++ follow-everybody.bash
@@ -0,0 +1,7 @@
+# Copyright @datn 2022. Heavens forgive me for this
+# be in your repo dir for this
+
+while IFS=/ read -r user repo; do
+	git remote add $user https://github.com/$user/$repo
+	git fetch $user
+done < <(curl https://api.github.com/repos/diracdeltas/tweets/forks | jq -r '.[].full_name')
